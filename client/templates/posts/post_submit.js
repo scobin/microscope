575d7d267c98788362456babf5f56892cb73bc35
ckscobin <ckscobin@gmail.com>
2015-05-16 09:17:53 +0000

    [modify] add comment

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -8,7 +8,20 @@ Template.postSubmit.events({
       message: $(e.target).find('[name=message]').val()
     }
     
-    post._id = Posts.insert(post);
-    Router.go('postPage', post);
+    Meteor.call('postInsert', // server method
+                post, // data
+                function(error, result) { // callback func.
+      // display the error to the user and abort
+      if (error)
+        return alert(error.reason);
+      // show this result but route anyway
+      if (result.postExists)
+        alert('This link has already been posted');
+
+    });
+    
+    
+    Router.go('postsList');
+//    Router.go('postPage', {_id: result._id});
   }
 });
