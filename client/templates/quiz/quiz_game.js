b035d18e69b9a6f8df0e3b56b848d48496e9ea86
scobin <ckscobin@gmail.com>
2015-06-24 23:51:00 +0900

    chnage the color when click the selection

--- client/templates/quiz/quiz_game.js
+++ client/templates/quiz/quiz_game.js
@@ -2,17 +2,24 @@ Template.quizGame.helpers({
   
 });
 
-checkAns = function(sel) {
-  if (Quizs.findOne().ans === sel) {
+checkAns = function(target) {
+  if (Quizs.findOne().ans === target.id) {
+    $(target).css('background', '#EE178C');
+    $(target).html(function() {
+      return "<b>○ </b> " + $(target).text(); 
+    });
     console.log('correct!');
   } else {
+    $(target).css('background', '#152A2D');
+    $(target).html(function() {
+      return "<b>× </b> " + $(target).text(); 
+    });
     console.log('wrong!');
   }
 }
 Template.quizGame.events({
   'click #sel1, click #sel2, click #sel3, click #sel4': function(e) {
     e.preventDefault();
-    console.log(e.target.id);
-    checkAns(e.target.id);
+    checkAns(e.target);
   }
 });
