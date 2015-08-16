da88167e07e4764a9dd22b22377bef17810817a8
scobin <ckscobin@gmail.com>
2015-08-16 02:58:14 +0000

    add test code for using http.call in client

--- client/templates/chat/chat.js
+++ client/templates/chat/chat.js
@@ -31,6 +31,34 @@ Template.chat.events({
         //call method
         var context = !!Session.get('context') ? Session.get('context') : null;
         var mode = !!Session.get('mode') ? Session.get('mode') : null;
+        var options = {
+          data: {
+            utt: text,
+            context: context,
+            mode: mode
+          }
+        };
+        // use async HTTP.post in Client.(for test)
+        // HTTP.post('https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY=7254667854504d6c6c3674766b4a497770536434434c752f666938565237724f433737304b4868726b7532',
+        // options,
+        // function(error, result) {
+        //   if (!error && result.statusCode == 200) {
+        //     var res_msgArray = Session.get('Messages');
+        //     res_msgArray.push({
+        //       'user': 'robot',
+        //       'text': result.data.utt,
+        //       'timestamp': Date.now()
+        //     });
+        //     Session.set('Messages', res_msgArray);
+        //     Session.set('context', result.data.context);
+        //     Session.set('mode', result.data.mode);
+        //   } else {
+        //     console.log('error: ' + result.statusCode);
+        //   }
+          
+        // });
+        
+        // use server method to get result.
         Meteor.call('talk', text, context, mode, function(error, result) {
           if (error) {
             throwError(error.reason);
